c77ed76ab2d768a6498ebd05acf34a0332674451
unknown <Josh@Josh-PC.(none)>
2013-11-14 23:17:37 +0800

    You can now build waypoints. Next up is the connection to organs.

---
+++ Assets/Scripts/Controllers/BuildController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildController : MonoBehaviour {
+
+	protected Vessel sourceVessel;
+	protected Plane xyZeroPlane;
+
+	void Start() {
+		xyZeroPlane = new Plane(Vector3.forward, Vector3.zero);
+	}
+	
+	void Update() {
+
+		if (Input.GetMouseButton(0)) {
+			if (Input.GetMouseButtonDown(0)) {
+				// Raycast
+				RaycastHit hit = Raycast(Input.mousePosition);
+				if (hit.transform != null) {
+					// It hit something!
+					CameraController.Instance.SetCamMovable(false);
+					sourceVessel = hit.transform.GetComponent<Vessel>();
+				} else {
+					// Nothing hit.
+					CameraController.Instance.SetCamMovable(true);
+					sourceVessel = null;
+				}
+			}
+
+			if (sourceVessel != null) {
+				Debug.Log("Vessel: " + sourceVessel.name);
+			}
+		}
+
+		if (Input.GetMouseButtonUp(0)) { 
+			if (sourceVessel != null) {
+				// Determine where to instantiate
+				Vector3 v = Vector3.zero;
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				float distance = 0;
+				if (xyZeroPlane.Raycast(ray, out distance)) 
+					v = ray.GetPoint(distance);
+
+				// Instantiate new segment
+				InstantiateVessel(sourceVessel, v);
+			} 
+
+			CameraController.Instance.SetCamMovable(true);
+			sourceVessel = null;
+		}
+	}
+
+	public void InstantiateVessel(Vessel attachedTo, Vector3 pos) {
+		GameObject g = Instantiate(Heart.Instance.vNodePrefab, pos, Quaternion.identity) as GameObject;
+		VesselEndpoint v = g.GetComponent<VesselEndpoint>();
+		v.name = "VesselEndpoint_" + GameController.vesselCounter++;
+		v.AttachVessel(attachedTo);
+	}
+
+	protected RaycastHit Raycast(Vector3 v) {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		
+		if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+			return hit;
+		return new RaycastHit();
+	}
+
+	/*
+	 * Singleton
+	 */
+	private static BuildController _instance;
+
+	public static BuildController Instance {
+		get { return _instance; }
+	}
+}
--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -2,24 +2,26 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
+	// Flags
+	private bool _allowCamMovement = true;
 
+	// References
 	protected Camera cam;
-	protected float dragSpeed = 22;
-	protected bool isMouseSet = false;
-	protected Vector3 prevMouseDown = Vector3.zero;
-	protected Plane xyZeroPlane;
 
 	// Screen Data
 	private float frustrumWidth;
 	private float frustrumHeight;
 
-	// TEST
+	// Movement Controls
+	protected bool isMouseSet = false;
 	protected Vector3 initCamDown;
 	protected Vector3 initMouseDown;
 
-	void Start() {
-		xyZeroPlane = new Plane(Vector3.forward, Vector3.zero);
+	void Awake() {
+		_instance = this;
+	}
 
+	void Start() {
 		cam = GetComponent<Camera>();
 		frustrumHeight = 2f * Mathf.Abs(transform.position.z) * Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
 		frustrumWidth = frustrumHeight * cam.aspect;
@@ -27,34 +29,15 @@ public class CameraController : MonoBehaviour {
 	}
 
 	void Update() {
-		if (Input.GetMouseButton(0)) {
-//			if (!isMouseSet) {
-//				isMouseSet = true;
-//				prevMouseDown = Input.mousePosition;
-//			}
-			
-//			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - prevMouseDown);
-//			Vector3 move = - new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
-//			transform.Translate(move, Space.World);  
-//			prevMouseDown = Input.mousePosition;
+		if (!_allowCamMovement) 
+			return;
 
-
-			// The right method!!!
-//			Vector3 v = Vector3.zero;
-//			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//			float distance = 0;
-//			if (xyZeroPlane.Raycast(ray, out distance)) 
-//				v = ray.GetPoint(distance);
-//			Debug.Log("V: " + v);
-			
+		if (Input.GetMouseButton(0)) {			
 			Vector3 viewportPt = cam.ScreenToViewportPoint(Input.mousePosition);
-			Debug.Log("Viewport: " + viewportPt);
 
 			// Init [mouseDownAt]
 			if (!isMouseSet) {
 				isMouseSet = true;
-//				prevMouseDown = v;
-
 				initCamDown = transform.position;
 				initMouseDown = viewportPt;
 			}
@@ -62,12 +45,26 @@ public class CameraController : MonoBehaviour {
 			// Move camera into the proper place
 			Vector3 delta = viewportPt - initMouseDown;
 			delta = -new Vector3(delta.x * frustrumWidth, delta.y * frustrumHeight, 0);
-			Debug.Log("Delta: " + delta);
+//			Debug.Log("Delta: " + delta);
 			transform.position = initCamDown + delta;
 		}
 
 		if (Input.GetMouseButtonUp(0)) {
 			isMouseSet = false;
 		}
+	}
+
+	public void SetCamMovable(bool b) {
+		_allowCamMovement = b;
+		if (b) {
+		} else {
+			isMouseSet = false;
+		}
+	}
+
+	private static CameraController _instance;
+
+	public static CameraController Instance {
+		get { return _instance; }
 	}
 }
