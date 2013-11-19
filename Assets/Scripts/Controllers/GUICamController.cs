f9cc0d63e6a2e2a0f28c2fd11760fa09ae8db40c
unknown <Josh@Josh-PC.(none)>
2013-11-19 21:25:57 +0800

    Control system changes.

--- Assets/Scripts/Controllers/GUICamController.cs
+++ Assets/Scripts/Controllers/GUICamController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GUICamController : MonoBehaviour {
 
@@ -9,7 +10,12 @@ public class GUICamController : MonoBehaviour {
 	// References
 	protected Camera _mainCamera;
 	protected Camera _guiCamera;
-	protected UnitsMenu _unitsMenu;
+	protected OrganUnitsMenu _organUnitsMenu;
+	protected SelectedUnitsMenu _selUnitsMenu;
+
+	//
+	protected List<BCell> _selectedCells = new List<BCell>();
+	protected List<BCell> _cells = new List<BCell>();
 
 	void Awake() {
 		_instance = this;
@@ -18,9 +24,9 @@ public class GUICamController : MonoBehaviour {
 	void Start() {
 		_mainCamera = CameraController.Instance.GetComponent<Camera>();
 		_guiCamera = GetComponent<Camera>();
-
-		_unitsMenu = transform.FindChild("UnitsMenu").GetComponent<UnitsMenu>();
-		_unitsMenu.gameObject.SetActive(false);
+		
+		_organUnitsMenu = transform.FindChild("OrganUnitsMenu").GetComponent<OrganUnitsMenu>();
+		_selUnitsMenu = transform.FindChild("SelectedUnitsMenu").GetComponent<SelectedUnitsMenu>();
 	}
 
 	void Update() {
@@ -30,25 +36,66 @@ public class GUICamController : MonoBehaviour {
 				Ray ray = _guiCamera.ScreenPointToRay(Input.mousePosition);
 				RaycastHit rayHit;
 				if (Physics.Raycast(ray, out rayHit, Mathf.Infinity, 1 << LayerMask.NameToLayer("GUI"))) {
-					if (rayHit.transform.name.StartsWith("BCellIdentifier_")) {
-						_unitsMenu.HitObject(rayHit.transform.gameObject);
+					string name = rayHit.transform.name;
+
+					if (name.StartsWith(OrganUnitsMenu.PREFIX)) {
+						// Clicked on Organ Units Menu
+						int index = _organUnitsMenu.IndexOfHit(rayHit.transform.gameObject);
+
+						// Open selected menu
+						if (_selectedCells.Count == 0) 
+							SetSelectedUnitsMenu(true);
+
+						_selectedCells.Add(_cells[index]);
+						_cells.Remove(_cells[index]);
+
+						_organUnitsMenu.UpdateUI(_cells.ToArray());
+						_selUnitsMenu.UpdateUI(_selectedCells.ToArray());
+					} else if (name.StartsWith(SelectedUnitsMenu.PREFIX)) {
+						// Clicked on Selected Units Menu
+						int index = _selUnitsMenu.IndexOfHit(rayHit.transform.gameObject);
+						_cells.Add(_selectedCells[index]);
+						_selectedCells.Remove(_selectedCells[index]);
+
+						// Close selected menu
+						if (_selectedCells.Count == 0) 
+							SetSelectedUnitsMenu(false);
+						_organUnitsMenu.UpdateUI(_cells.ToArray());
+						_selUnitsMenu.UpdateUI(_selectedCells.ToArray());
+					} else if (name.StartsWith("ExitMenu")) {
+
 					}
 				} else {
+					bool keepOpen = false;
+
 					ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
 					if (Physics.Raycast(ray, out rayHit)) {
 						Organ org = rayHit.transform.GetComponent<Organ>();
 
 						if (org != null) {
 							if (org != _activeOrgan && org != Heart.Instance) {
-								if (BCell.HasPathTo(Heart.Instance, org)) {
-									_unitsMenu.SendUnitsTo(org);
-								}
+								if (BCell.HasPathTo(Heart.Instance, org)) 
+									SendUnitsTo(org);
 							}
 						} else {
-							SetUnitsMenu(false);
+							// Hit Vessel
+							if (_selectedCells.Count > 0) 
+								keepOpen = true;
 						}
 					} else {
+						// Hit Nothing.
+						if (_selectedCells.Count > 0) 
+							keepOpen = true;
+					}
+
+					if (keepOpen) {
+						// 1) Close OrganUnitsMenu()
+						// 2) Don't Close SelectedUnitsMenu()
+
+					} else {
+						// Close Menu
 						SetUnitsMenu(false);
+						SetSelectedUnitsMenu(false);
 					}
 				}
 			}
@@ -56,23 +103,39 @@ public class GUICamController : MonoBehaviour {
 			if (_activeOrgan != null) {
 				Vector3 viewportPoint = _mainCamera.WorldToViewportPoint(_activeOrgan.transform.position);
 				Ray r = _guiCamera.ViewportPointToRay(viewportPoint);
-				_unitsMenu.transform.position = r.GetPoint(1f); 
+				_organUnitsMenu.transform.position = r.GetPoint(1f); 
 			}
 		}
+	}
+	
+	public void SendUnitsTo(Organ org) {
+		for (int i = 0; i < _selectedCells.Count; i++) 
+			org.OnRequestCell(_selectedCells[i]);
+		_selectedCells.Clear();
 	}
 
 	public void OpenOrganMenu(Organ org) {
 		_activeOrgan = org;
-		_unitsMenu.SetOrgan(org);
+		_cells = CellController.Instance.GetCellsAt(org);
+		_selectedCells.Clear();
+
+		BCell[] b = _cells.ToArray();
+		Debug.Log("Cells: " + b.Length);
 
 		if (org != null) {
-			SetUnitsMenu(true);
+			_organUnitsMenu.gameObject.SetActive(true);
+			_organUnitsMenu.UpdateUI(_cells.ToArray());
 		} else {
 		}
 	}
 
 	public void SetUnitsMenu(bool b) {
-		_unitsMenu.gameObject.SetActive(b);
+		_organUnitsMenu.gameObject.SetActive(b);
+		BuildController.Instance.isBuildMode = !b;
+	}
+
+	public void SetSelectedUnitsMenu(bool b) {
+		_selUnitsMenu.gameObject.SetActive(b);
 		BuildController.Instance.isBuildMode = !b;
 	}
 
--- Assets/Scripts/Controllers/OrganUnitsMenu.cs
+++ Assets/Scripts/Controllers/OrganUnitsMenu.cs
@@ -1,73 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
-public class UnitsMenu : MonoBehaviour {
+public class OrganUnitsMenu : MonoBehaviour {
 
+	public const string PREFIX = "OrganUnit_";
 	protected Organ _activeOrgan;
-	protected GameObject[] unSelectedObjects;
-
-	//
-	protected GUICamController _guiController;
-	protected SelectedUnitsMenu _selectedUnitsMenu;
+	protected GameObject[] cellObjects;
 
 	void Start() {
-		// References
-		_guiController = transform.root.GetComponentInChildren<GUICamController>();
-		_selectedUnitsMenu = transform.root.GetComponentInChildren<SelectedUnitsMenu>();
-
 		// Instantiate lots of stuff.
-		GameObject gUnselected = transform.FindChild("UnitUnselected").gameObject;
-		unSelectedObjects = new GameObject[Organ.MAX_CELLS];
+		GameObject prefab = transform.FindChild("UnitUnselected").gameObject;
+		cellObjects = new GameObject[Organ.MAX_CELLS];
 		int c = 0;
 		for (int o = 0; o < 2; o++) {
 			for (int i = 0; i < 4; i++) {
-				Vector3 pos = gUnselected.transform.position + new Vector3(gUnselected.transform.localScale.x * i, gUnselected.transform.localScale.y * -o, 0);
-				GameObject tg = Instantiate(gUnselected, pos, Quaternion.identity) as GameObject;
-				tg.name = "BCellIdentifier_" + (o * 4 + i);
-				tg.transform.parent = gUnselected.transform.parent;
+				Vector3 pos = prefab.transform.position + new Vector3(prefab.transform.localScale.x * i, prefab.transform.localScale.y * -o, 0);
+				GameObject tg = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
+				tg.name = PREFIX + (o * 4 + i);
+				tg.transform.parent = prefab.transform.parent;
 				tg.SetActive(false);
 
-				unSelectedObjects[c++] = tg;
+				cellObjects[c++] = tg;
 			}
 		}
 		
-		Destroy(gUnselected);
+		Destroy(prefab);
+		gameObject.SetActive(false);
 	}
 
-	public void HitObject(GameObject g) {
+	public int IndexOfHit(GameObject g) {
 		if (g != null) {
-			for (int i = 0; i < unSelectedObjects.Length; i++) {
-				if (unSelectedObjects[i] == g) {
-					_selectedUnitsMenu.SetCellSelected(_cells[i]);
-					_cells.Remove(_cells[i]);
+			for (int i = 0; i < cellObjects.Length; i++) {
+				if (cellObjects[i] == g) {
+					return i;
 				}
 			}
 		}
-		UpdateUI();
+		return -1;
 	}
 
-	public void SetOrgan(Organ org) {
-		gameObject.SetActive(org != null);
-
-		_activeOrgan = org;
-		_cells = CellController.Instance.GetCellsAt(org);
-		_selectedUnitsMenu.Clear();
-		UpdateUI();
-	}
-	
-	public void SetCellUnSelected(BCell b) {
-		_cells.Add(b);
-		UpdateUI();
-	}
-
-	public void UpdateUI() {
+	public void UpdateUI(BCell[] cells) {
 		// Disable all objects
-		for (int i = 0; i < unSelectedObjects.Length; i++) 
-			unSelectedObjects[i].SetActive(false);
+		for (int i = 0; i < cellObjects.Length; i++) 
+			cellObjects[i].SetActive(false);
 
 		// Enable select objects
-		for (int i = 0; i < _cells.Count; i++) 
-			unSelectedObjects[i].SetActive(true);
+		for (int i = 0; i < cells.Length; i++) 
+			cellObjects[i].SetActive(true);
 	}
 }
